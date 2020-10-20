bf46713ea38c3b551e0a0b7941bbc1ea2f9cdbb7
leticiacostadias <leticia.costadias@gmail.com>
2020-10-20 09:38:24 -0300

    Initial config

---
+++ src/pages/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Head from 'next/head'
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <Head>
+        <title>Dota graphs</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>Dota graphs</h1>
+      </main>
+    </div>
+  )
+}
+
+export default Home
